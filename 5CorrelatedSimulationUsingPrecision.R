f081d451e71e55852f2293ce00e0f75b210347f3
Widya Pranayoga <58467498+zaresilkwood@users.noreply.github.com>
2020-09-04 00:59:53 +0100

    Add files via upload

--- 5CorrelatedSimulationUsingPrecision.R
+++ 5CorrelatedSimulationUsingPrecision.R
@@ -3,51 +3,11 @@ library(tidyverse)
 library(ggplot2)
 library(MASS)
 library(LaplacesDemon)
-library(clusterGeneration)
 
+#Create list for markov chain 0 precision
 
-n <- 5
-sigmaall<-matrix(rdunif(n^2, 0, 8), ncol=n)
-sigmaall[lower.tri(sigmaall)] = t(sigmaall)[lower.tri(sigmaall)]
-diag(sigmaall) <- rep(50, n)
-sigmaall
-
-
-library(pracma)
-k = 5
-sparsity = .2
-# generate the symmetric sparsity mask
-mask = rand(k)
-mask = mask * (mask < sparsity)
-mask[lower.tri(mask, diag = TRUE)] = 0
-mask = mask + t(mask) + eye(k)
-mask[mask > 0] = 1
-
-# generate the symmetric precision matrix
-theta = matrix(rnorm(k^2), k)
-theta[lower.tri(theta, diag = TRUE)] = 0
-theta = theta + t(theta) + eye(k)
-
-# apply the reqired sparsity
-theta = theta * mask
-
-# force it to be positive definite
-theta = theta - (min(eig(theta))-.1) * eye(k)
-theta
-
-Prec2Cov(theta)
-
-
-n <- 5
-prectest<-matrix(runif(n^2, 0, 1), ncol=n)
-prectest[lower.tri(prectest)] = t(prectest)[lower.tri(prectest)]
-diag(prectest) <- rep(1, n)
-prectest
-
-Prec2Cov(prectest)
 nolist <- c(4,5,8,10,15)
 
-#FROM THIS THE CODE START!!!
 
 x <- matrix(runif(25,-1,1), nrow = 5)
 x[lower.tri(x)] = t(x)[lower.tri(x)]
@@ -62,26 +22,6 @@ for (i in 1:5) {
     }
   }
 }
-
-x
-Prec2Cov(x)
-
-
-x <- matrix(runif(25,-1,1), nrow = 5)
-x[lower.tri(x)] = t(x)[lower.tri(x)]
-diag(x) <- runif(5,3,4)
-for (i in 1:5) {
-  for (j in 1:5) {
-    if (i == j){
-      next
-    }
-    if ((i*j) %in% nolist) {
-      x[i,j] <- 0
-    }
-  }
-}
-
-x
 
 prec_matrix <- data.frame(x) %>% 
   round(3)
@@ -167,32 +107,3 @@ for (i in 1:10) {
 }
 
 compareplot_imp(cornorm1a,cornorm2a,"corelatednorm_usingprec1desc.png")
-
-cor(group2[1:5])
-#minimum correlation data works fine.
-
-#Explore this next time??
-Posdef <- function (n, ev = runif(n, 0, 10)){
-  Z <- matrix(ncol=n, rnorm(n^2))
-  decomp <- qr(Z)
-  Q <- qr.Q(decomp) 
-  R <- qr.R(decomp)
-  d <- diag(R)
-  ph <- d / abs(d)
-  O <- Q %*% diag(ph)
-  Z <- t(O) %*% diag(ev) %*% O
-  return(Z)
-}
-
-pdmat <- Posdef(n=5, ev=1:5)
-eigen(pdmat)$val
-pdmat <- pdmat %*% t(pdmat)
-Prec2Cov(pdmat)
-
-genPositiveDefMat("unifcorrmat",dim=4) 
-x <- genPositiveDefMat("eigen",dim=5, rangeVar = c(1,1))$Sigma
-x <- x %*% t(x)
-str(x)
-Prec2Cov(x)
-matrix(x,ncol = 5)
-genPositiveDefMat()
